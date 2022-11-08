bba01795b77751862a1cb4bf072bdb667b1336c7
marimart <marimart@penguin>
2022-11-08 08:00:08 -0500

    added associations

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const {Board} = require('./Board');
 const {Cheese} = require('./Cheese');
 const {User} = require('./User');
 
-
-
+Board.belongsTo(User);
+User.hasMany(Board)
+Cheese.belongsTo(Board);
+Board.hasMany(Cheese);
 
 module.exports = {Board, Cheese, User}
